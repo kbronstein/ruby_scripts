e6d34df6f2e88b91e04f82225a9fdc02ee71e86a
Seth <dolinseth@gmail.com>
2013-04-15 15:36:21 -0400

    modified factorial class

--- Maths/Factorial.rb
+++ Maths/Factorial.rb
@@ -1,15 +1,27 @@
 class Factorial
 	def self.calculate(number)
 		@number = number
+		@answer = @number
 		while @number > 1
-			@answer = @number
-			@answer *= @number
 			@number -= 1
+			@answer *= @number
 		end
 		@answer
 	end
 
+	def self.calculates(number)
+		@number = number
+		@answer = @number
+		while @number > 1
+			@number -= 1
+			@answer *= @number
+			puts "#{@answer}"
+		end
+		@answer		
+	end
+
 	def self.help
 		puts "calculate(number)"
+		puts "calculates(number)"
 	end
 end
